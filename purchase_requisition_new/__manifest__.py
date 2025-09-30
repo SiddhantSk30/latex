c9be900f7d99ffd9874c38c77bab8264c1dc395f
SiddhantSk30 <siddhantkakade12345@gmail.com>
2025-09-30 14:11:22 +0530

    error fixed on purchase_requisition_new module

--- purchase_requisition_new/__manifest__.py
+++ purchase_requisition_new/__manifest__.py
@@ -8,12 +8,11 @@
     'depends': ['base', 'mail', 'purchase', 'product'],
     'data': [
         'security/groups.xml',
-        'security/purchase_requisition_rules.xml',   # added rules here
+        'security/purchase_requisition_rules.xml',
         'data/sequence.xml',
         'views/purchase_requisition_menu.xml',
         'views/purchase_requisition_action.xml',
         'views/purchase_requisition_view.xml',
-        # Access rights must be last so models are loaded before CSV is processed
         'security/ir.model.access.csv',
     ],
     'installable': True,
